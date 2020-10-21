8320e6925bf74f5924621c3ee59aee29971894b9
stevenoakley211 <stevenoakley211@live.com>
2020-10-20 20:16:25 -0500

    hold onto your butts

--- knexfile.js
+++ knexfile.js
@@ -31,11 +31,9 @@ module.exports = {
 
   production: {
     client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
+    connection: 
+      process.env.DATABASE_URL + '?ssl=true'
+    ,
     pool: {
       min: 2,
       max: 10
