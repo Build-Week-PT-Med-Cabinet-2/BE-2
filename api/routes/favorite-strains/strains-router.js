af7ca275b72ef0010463418e176da5c84672bf98
stevenoakley211 <stevenoakley211@live.com>
2020-10-21 17:41:30 -0500

    updated error handling

--- api/routes/favorite-strains/strains-router.js
+++ api/routes/favorite-strains/strains-router.js
@@ -6,38 +6,28 @@ const bcrypt = require('bcrypt')
 
 router.get('/', (req, res) => {
     let id = req.decodedJwt.userid
-    console.log(id)
     strains.findById(id)
-        .then(
-            response => res.status(200).json(response)
-            
-        )
-        .catch(
-            err => res.status(400).json(err.message)
-        )
+        .then(response => res.status(200).json(response))
+        .catch(res.status(500).json({error: err.message}))
 })
 
 router.post('/', (req, res) =>{ 
     let newBody = Object.assign(req.body,{'user_id': req.decodedJwt.userid})
-    console.log(newBody)
     strains.add(newBody)
         .then( response => res.status(200).json(response))
-        .catch(
-            err => res.status(404).json(err.message)
-        )
+        .catch(err => res.status(500).json({error: err.message}))
 })
 
 router.put('/:id', (req, res) => {
     strains.update(req.params.id,req.body)
         .then(response => res.status(200).json(response))
-        .catch(err => res.status(404).json(err.message))
+        .catch(err => res.status(500).json({error: err.message}))
 })
 
 router.delete('/:id', (req, res) => {
     strains.remove(req.params.id)
         .then(response=> res.status(200).json({message:`Strain #${req.params.id} Deleted`}))
-        .catch(err => res.status404.json
-    )
+        .catch(err => res.status(500).json({error: err.message}))
 })
 
 module.exports = router
--- api/routes/user/user-router.js
+++ api/routes/user/user-router.js
@@ -18,25 +18,21 @@ router.put('/',(req,res)=> {
         let hashed = bcrypt.hashSync(req.body.password,10)
         let newPassword = {'password': hashed}
         user.update(id, newPassword)
-            .then(success => {
-                res.status(200).json(success)
-            })
-
+            .then(success => {res.status(200).json(success)})
+            .catch(err => res.status(500).json({error: err.message}))
     }
     else{
         user.update(id, req.body)
-        .then(
-                success => res.status(200).json(success)
-            )
+        .then(success => res.status(200).json(success))
+        .catch(err => res.status(500).json({error: err.message}))
     }
 })
 
 router.delete('/', (req,res) => {
     let id = req.decodedJwt.userid
     user.remove(id)
-        .then(
-            success => res.status(200).json({message: "user deleted"})
-        )
+        .then(success => res.status(200).json({message: "user deleted"}))
+        .catch(err => res.status(500).json({error: err.message}))
 })
 
 module.exports = router
