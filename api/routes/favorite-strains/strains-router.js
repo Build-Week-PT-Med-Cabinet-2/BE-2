d8da8694557f4370b685bff599efb42514594323
stevenoakley211 <stevenoakley211@live.com>
2020-10-24 20:28:08 -0500

    try 2

--- api/routes/favorite-strains/strains-router.js
+++ api/routes/favorite-strains/strains-router.js
@@ -4,17 +4,17 @@ const strains = require('./strains-model')
 const jwt = require('jsonwebtoken')
 const bcrypt = require('bcrypt')
 
-// function filter (item) {
-//     const allowed = ['Ailment','Description','Effects_x','Effects_y','Flavor','Rating','Strain','Type']
-//     let filtered = Object.keys(item)
-//         .filter(key => allowed.includes(key))
-//         .reduce((obj, key) => {
-//             obj[key] = item[key];
-//             return obj;
-//           }, {})
-//         console.log(filtered)
-//         return filtered 
-// }
+function filter (item) {
+    const allowed = ['Ailment','Description','Effects_x','Effects_y','Flavor','Strain','Type']
+    let filtered = Object.keys(item)
+        .filter(key => allowed.includes(key))
+        .reduce((obj, key) => {
+            obj[key] = item[key];
+            return obj;
+          }, {})
+        console.log(filtered)
+        return filtered 
+}
 
 router.get('/', (req, res) => {
     let id = req.decodedJwt.userid
@@ -24,8 +24,8 @@ router.get('/', (req, res) => {
 })
 
 router.post('/', (req, res) =>{ 
-        // let firstBody = filter(req.body)
-        let newBody = Object.assign(req.body,{'user_id': req.decodedJwt.userid})
+        let firstBody = filter(req.body)
+        let newBody = Object.assign(firstBody,{'user_id': req.decodedJwt.userid})
         // delete newBody.Unnamed;
         // delete newBody.id;
         // delete newBody.fitness;
